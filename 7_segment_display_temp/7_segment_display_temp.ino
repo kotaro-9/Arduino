32cd77e7ca0fe45547fee879465b9cbd4462fc1e
k0taro9 <126588189+k0taro9@users.noreply.github.com>
2024-01-22 12:04:11 +0100

    update files

--- 7_segment_display_temp/7_segment_display_temp.ino
+++ 7_segment_display_temp/7_segment_display_temp.ino
@@ -2,10 +2,19 @@ int SDI = 3;
 int SCLK = 5;
 int LOAD = 6;
 
-int zahl[16];
+double sensorValue = 0;
+double sensorU = 0;
+double sensorR = 0;
+double tempK = 0;
+double tempC = 0;
+double analogPin = A0;
+
+int temp = 0;
+int einer = 0;
+int zehner = 0;
 
 // Low-Aktiv - 10 Ziffern mit jeweils 8 Bits
-int ziffern[11][8] = { 
+int ziffern[12][8] = { 
   { 1, 1, 0, 0, 0, 0, 0, 0 }, // 0
   { 1, 1, 1, 1, 1, 0, 0, 1 }, // 1
   { 1, 0, 1, 0, 0, 1, 0, 0 }, // 2
@@ -16,7 +25,8 @@ int ziffern[11][8] = {
   { 1, 1, 1, 1, 1, 0, 0, 0 }, // 7
   { 1, 0, 0, 0, 0, 0, 0, 0 }, // 8
   { 1, 0, 0, 1, 0, 0, 0, 0 }, // 9
-  { 1, 0, 1, 1, 1, 1, 1, 1 }  // -
+  { 1, 0, 1, 1, 1, 1, 1, 1 }, // -
+  { 1, 1, 1, 1, 1, 1, 1, 1 }  // /
 };
 
 void setup() {
@@ -25,52 +35,93 @@ void setup() {
   pinMode(LOAD, OUTPUT);
 }
 
-void to16Bits(int nummer) {
-  if (nummer >= 0 && nummer <= 99) { // 0 - 99
-    for (int i = 0; i < 8; i++) {
-      zahl[i] = ziffern[nummer / 10][i];
-      zahl[i + 8] = ziffern[nummer % 10][i];
-    }
-    sendToDisplay();
-  } else if (nummer <= -1 && nummer >= -9) { // -1 - -9
-    for (int i = 0; i < 8; i++) {
-      zahl[i] = ziffern[-1][i];
-      zahl[i + 8] = ziffern[(nummer * (-1))]; 
-    }
-    sendToDisplay();
-  } else if (nummer > 99) { // >99
-    for (int i = 0; i < 8; i++) {
-      zahl[i] = ziffern[9][i];
-      zahl[i + 8] = ziffern[9][i];
-    }
-    sendToDisplay();
-  } else if (nummer < -9) { // <-9
-    for (int i = 0; i < 8; i++) {
-      zahl[i] = ziffern[-1][i];
-      zahl[i + 8] = ziffern[9][i];
-    }
-    sendToDisplay();
+void calcTemp() {
+  sensorValue = analogRead(analogPin);
+  sensorU = (5 * sensorValue) / 1023; // Spannung des Sensors
+  sensorR = (sensorValue * 10000) / (1023 - sensorValue); // Berechnet den Widerstand des Sensors
+  // temp in Kelvin
+  tempK = pow(3.354016 *
+    pow(10, -3) + 2.569850 *
+    pow(10, -4) *
+    log(sensorR / 10000) +
+    2.62013 * pow(10, -12) *
+    pow(log(sensorR / 10000), 2) +
+    6.38309 * pow(10, -15) *
+    pow(log(sensorR / 10000), 3), -1);
+  tempC = tempK - 273.15; // Konvertiert die Temperatur in Celsius
+  temp = round(tempC);
+  if (temp < 0 && temp > -10) {
+    negTemp();
+  } else if (-9 > temp) {
+    negLowerTemp();
+  } else {
+    posTemp();
   }
 }
 
-void sendToDisplay() {
-  digitalWrite(LOAD, LOW); // LOAD auf LOW, um Latch-Vorgang zu aktivieren - Speichermodus
-  for (int i = 0; i < 16; i++) { // Schleife fuer 16 Bit Muster
+void posTemp() {
+  digitalWrite(LOAD, LOW);
+  // einer
+  for (int i = 0; i < 8; i++) {
     digitalWrite(SCLK, LOW);
-    digitalWrite(SDI, zahl[i]);
-    Serial.print(zahl[i]); // Kontrolle
+    digitalWrite(SDI, ziffern[temp % 10][i]);
     digitalWrite(SCLK, HIGH);
   }
-  Serial.println("\n"); // Kontrolle
+  // zehner
+  for (int i = 0; i < 8; i++) {
+    digitalWrite(SCLK, LOW);
+    digitalWrite(SDI, ziffern[temp / 10][i]);
+    digitalWrite(SCLK, HIGH);
+  }
   digitalWrite(LOAD, HIGH);
 }
 
-void loop() {
-  for(int i = 0; i <= 99; i++) {
-    to16Bits(i);
-    delay(1000);
+void negTemp() {
+  digitalWrite(LOAD, LOW);
+  for (int i = 0; i < 8; i++) {
+    digitalWrite(SCLK, LOW);
+    digitalWrite(SDI, ziffern[temp * (-1)][i]);
+    digitalWrite(SCLK, HIGH);
   }
+  for (int i = 0; i < 8; i++) {
+    digitalWrite(SCLK, LOW);
+    digitalWrite(SDI, ziffern[10][i]);
+    digitalWrite(SCLK, HIGH);
+  }
+  digitalWrite(LOAD, HIGH);
 }
 
-// temp erfassung und berechnung einbauen
-// erfasste temp in to16Bits() eingeben
+void negLowerTemp() {
+  digitalWrite(LOAD, LOW);
+  for (int i = 0; i < 8; i++) {
+    digitalWrite(SCLK, LOW);
+    digitalWrite(SDI, ziffern[11][i]);
+    digitalWrite(SCLK, HIGH);
+  }
+  for (int i = 0; i < 8; i++) {
+    digitalWrite(SCLK, LOW);
+    digitalWrite(SDI, ziffern[10][i]);
+    digitalWrite(SCLK, HIGH);
+  }
+  digitalWrite(LOAD, HIGH);
+  delay(250);
+  digitalWrite(LOAD, LOW);
+  // einer
+  for (int i = 0; i < 8; i++) {
+    digitalWrite(SCLK, LOW);
+    digitalWrite(SDI, ziffern[(temp * (-1)) % 10][i]);
+    digitalWrite(SCLK, HIGH);
+  }
+  // zehner
+  for (int i = 0; i < 8; i++) {
+    digitalWrite(SCLK, LOW);
+    digitalWrite(SDI, ziffern[(temp * (-1)) / 10][i]);
+    digitalWrite(SCLK, HIGH);
+  }
+  digitalWrite(LOAD, HIGH);
+  delay(500);
+}
+
+void loop() {
+  calcTemp();
+}
