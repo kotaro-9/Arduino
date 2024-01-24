39b9b5ee027edc8e3717c1a810e7ee3750a99f44
k0taro9 <126588189+k0taro9@users.noreply.github.com>
2024-01-24 09:20:53 +0100

    Update rgb_led.ino

--- rgb_led/rgb_led.ino
+++ rgb_led/rgb_led.ino
@@ -4,16 +4,32 @@ int bluePin = 9;
 
 void setup() {
 	pinMode(redPin, OUTPUT);
-	pinMode(greenPin, OUTPUT);
 	pinMode(bluePin, OUTPUT);
+	pinMode(greenPin, OUTPUT);
 }
 
-void setColor(int red, int green, int blue) {
+void userInput() {
+  Serial.begin(4800);
+  int red = Serial.parseInt();
+  int blue = Serial.parseInt();
+  int green = Serial.parseInt();
+
+  // Werte zwischen 0-255
+  red = constrain(red, 0, 255);
+  green = constrain(green, 0, 255);
+  blue = constrain(blue, 0, 255);
+
+  setColor(red, blue, green);
+}
+
+void setColor(int red, int blue, int green) {
 	analogWrite(redPin, red);
-	analogWrite(greenPin, green);
-	analogWrite(bluePin, blue);
+	analogWrite(bluePin, green);
+	analogWrite(greenPin, blue);
 }
 
 void loop() {
-  setColor(255, 0, 255);
+  setColor(255, 255, 0);
+  userInput();
+  delay(1000);
 }
